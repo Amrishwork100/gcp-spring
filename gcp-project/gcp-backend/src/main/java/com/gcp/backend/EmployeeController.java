4b7cc762c8deba974cb865569ac763b6c06c16e7
@amrish12 <shiv.amrish2011@gmail.com>
2019-12-19 16:14:34 +0530

    d

--- gcp-project/gcp-backend/src/main/java/com/gcp/backend/EmployeeController.java
+++ gcp-project/gcp-backend/src/main/java/com/gcp/backend/EmployeeController.java
@@ -53,7 +53,7 @@ public class EmployeeController {
         return ResponseEntity.ok().body(employee);
     }
     
-    @GetMapping("/employees")
+    @GetMapping("/")
     public Employee createEmployee() {
 		
 		Employee employee=new Employee();
